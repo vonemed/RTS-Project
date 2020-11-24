11152535eacd2f3b02df99e6ef82151f125365db
polezniy <glebadem@gmail.com>
2020-11-24 17:24:57 +0300

    Added CameraController

---
+++ CameraController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private const float cameraSpeed = 100f;
+    public float rotationSpeed = 50f;
+    public float zoomSpeed = 200f;
+
+    [Header("Camera Limitations")]
+    public Vector2 xAxis;
+    public Vector2 zAxis;
+    public Vector2 zoomLimit;
+
+    private Vector3 cameraPos; // Current position of camera
+
+    void Update()
+    {
+        cameraPos = transform.position;
+
+        #region CameraMovement
+        if (Input.GetKey(KeyCode.W))
+        {
+            cameraPos.z += cameraSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            cameraPos.x -= cameraSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            cameraPos.z -= cameraSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            cameraPos.x += cameraSpeed * Time.deltaTime;
+        }
+        #endregion
+        #region CameraRotation
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(transform.up, -rotationSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(transform.up, rotationSpeed * Time.deltaTime);
+        }
+
+        #endregion
+        #region CameraZoom
+
+        float currentZoom = Input.GetAxis("Mouse ScrollWheel");
+
+        cameraPos.y -= currentZoom * zoomSpeed * Time.deltaTime;
+
+        #endregion
+
+        // Movement limitations
+        /*cameraPos.x = Mathf.Clamp(cameraPos.x, xAxis.x, xAxis.y);
+        cameraPos.z = Mathf.Clamp(cameraPos.z, zAxis.x, zAxis.y);
+        // Zoom limitations
+        cameraPos.y = Mathf.Clamp(cameraPos.y, zoomLimit.x, zoomLimit.y);*/
+
+        transform.position = cameraPos;
+    }
+}
