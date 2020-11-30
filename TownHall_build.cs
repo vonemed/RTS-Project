927c3bfc658bdddd6d04ebdc5adb5444404e75d4
polezniy <glebadem@gmail.com>
2020-11-30 13:45:06 +0300

    Added a TownHall building script

---
+++ TownHall_build.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TownHall_build : MonoBehaviour
+{
+    public GameObject townhall_blueprint;
+
+    public void SpawnBlueprint()
+    {
+        Instantiate(townhall_blueprint);
+    }
+}
