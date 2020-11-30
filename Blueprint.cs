a6c78e314ec803222d6946efa1b07c5cb3828dbd
polezniy <glebadem@gmail.com>
2020-11-30 13:36:56 +0300

    Added blueprint buildings placement

---
+++ Blueprint.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Blueprint : MonoBehaviour
+{
+
+    RaycastHit hit;
+    Vector3 movePooint;
+    public GameObject prefab;
+
+    void Start()
+    {
+       
+    }
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(ray.origin, ray.direction * 10, Color.red);
+        if (Physics.Raycast(ray, out hit, 50000.0f,1 << 8))
+        {
+            transform.position = hit.point;
+        }
+        //Destroy the blueprint once left mouse button is pressed
+        if (Input.GetMouseButton(0))
+        {
+            Instantiate(prefab, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
