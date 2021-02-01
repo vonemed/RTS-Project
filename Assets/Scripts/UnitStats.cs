c9f6fffa638b12142a76218f05a720d19696d147
polezniy <glebadem@gmail.com>
2021-02-01 18:36:39 +0300

    Preparing for implementation of gathering mechanic

--- Assets/Scripts/UnitStats.cs
+++ Assets/Scripts/UnitStats.cs
@@ -29,13 +29,24 @@ public class UnitStats : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButton(1))
+        if (GetComponent<Selected>())
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            if (Physics.Raycast(ray, out hit, 50000.0f))
+            if (Input.GetMouseButton(1))
             {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
+                Debug.DrawRay(ray.origin, ray.direction * 100, Color.yellow);
+
+                if (Physics.Raycast(ray, out hit, 50000.0f))
+                {
+                    if(hit.collider.name == "GoldMine")
+                    {
+                        Debug.Log("WE GOT GOLD!");
+                        //Check the distance to the mine
+                        //If the unit is close enough start gathering resource
+                        //Access resource UI and start to add resource at specific rate and deducting it from resource stats of selected resource deposit
+                    }
+                }
             }
         }
     }
