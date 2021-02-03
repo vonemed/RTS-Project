c16bc59772b599db6dbc12490c01f0ad014848f0
polezniy <glebadem@gmail.com>
2021-02-03 18:42:04 +0300

    Deleted more useless scripts

--- Assets/Scripts/BuildingsCreator.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-public class BuildingsCreator : MonoBehaviour
-{
-    public BuildingStats townHall;
-    public BuildingBlueprint barracks;
-    public BuildingBlueprint specialBuilding;
-}
