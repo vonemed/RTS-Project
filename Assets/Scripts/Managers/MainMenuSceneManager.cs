04fd9083e253a7dc018dfd33f0544f80fc32cd28
polezniy <glebadem@gmail.com>
2021-02-08 13:36:18 +0300

    Added a scene manager for main menu and created a subfolder

---
+++ Assets/Scripts/Managers/MainMenuSceneManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuSceneManager : MonoBehaviour
+{
+    public Button play;
+    public Button editor;
+    public Button options;
+    public Button exit;
+
+    void Start()
+    {
+        play.onClick.AddListener(playScene);
+        editor.onClick.AddListener(editorScene);
+        options.onClick.AddListener(optionsScene);
+        exit.onClick.AddListener(exitScene);
+    }
+
+    void playScene()
+    {
+        SceneManager.LoadScene("GamePlay");
+    }
+
+    void editorScene()
+    {
+        SceneManager.LoadScene("GameEditor");
+    }
+
+    void optionsScene()
+    {
+        SceneManager.LoadScene("Options");
+    }
+
+    void exitScene()
+    {
+        Application.Quit(0);
+    }
+}
