99f23550c26df4c25103563b3f61178c4f48ea37
polezniy <glebadem@gmail.com>
2021-02-08 14:02:12 +0300

    Adding meta files and created another subfolder

---
+++ Assets/Scripts/Stats/ResourceStats.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ResourceStats : MonoBehaviour
+{
+    [Header("Parameters")]
+    public string nameTag;
+    public bool gold;
+    public bool wood;
+    public bool mineral;
+
+    [Header("Utility")]
+    public int goldNum;
+    public int woodNum;
+    public int mineralNum;
+
+    void Start()
+    {
+        if(gold)
+        {
+            goldNum = 1000;
+        }
+
+        if(wood)
+        {
+            woodNum = 300;
+        }
+
+        if(mineral)
+        {
+            mineralNum = 100;
+        }
+    }
+}
