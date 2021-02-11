4de59c1d7b73d8e7c1cf466bf47b1e428e831600
polezniy <glebadem@gmail.com>
2021-02-11 16:43:16 +0300

    Added Animations Manager

---
+++ Assets/Scripts/Managers/AnimationsManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class AnimationsManager : MonoBehaviour
+{
+    public RuntimeAnimatorController idle;
+    public RuntimeAnimatorController sprint;
+    public RuntimeAnimatorController gather;
+    Animator animationController;
+    public int currentId;
+
+    void Start()
+    {
+        animationController = GetComponent<Animator>();
+        currentId = 0;
+    }
+
+    public void setAnimation(int _id)
+    {
+        currentId = _id;
+        if(_id == 1)
+        {
+            //idle
+            animationController.runtimeAnimatorController = idle;
+        }
+
+        if(_id == 2)
+        {
+            //sprint
+            animationController.runtimeAnimatorController = sprint;
+        }
+
+        if(_id == 3)
+        {
+            //gather
+            animationController.runtimeAnimatorController = gather;
+        }
+    }
+
+    public int getAnimation()
+    {
+        return currentId;
+    }
+    
+}
