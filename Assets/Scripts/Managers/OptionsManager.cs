875a1cecee98bfa9a732656bd41fa4fd0c63567b
polezniy <glebadem@gmail.com>
2021-02-12 13:49:48 +0300

    Added button manager for Options scene

---
+++ Assets/Scripts/Managers/OptionsManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class OptionsManager : MonoBehaviour
+{
+    public Button back;
+    void Start()
+    {
+        back.onClick.AddListener(backAction);
+    }
+
+    void backAction()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
