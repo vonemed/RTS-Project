488d4d806729a00495bd5d9f1bd121512ee681f5
Gleb Demenov <s4830315@bournemouth.ac.uk>
2021-04-23 18:59:50 +0300

    Preparings for implementation of save system

---
+++ Assets/Scripts/Save.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Save 
+{
+    public List<int> existingObjects = new List<int>();
+    public List<int> existingObjectsPosition = new List<int>();
+
+
+}
