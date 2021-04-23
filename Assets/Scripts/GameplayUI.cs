2b9c25b4e47fc964af9c2957cff86e002accc3a4
Gleb Demenov <s4830315@bournemouth.ac.uk>
2021-04-23 09:35:04 +0300

    Added back button for main gameplay loop

---
+++ Assets/Scripts/GameplayUI.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class GameplayUI : MonoBehaviour
+{
+    Button menuButton;
+    void Start()
+    {
+        menuButton = GetComponent<Button>();
+        menuButton.onClick.AddListener(mainMenu);
+    }
+
+    void mainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
